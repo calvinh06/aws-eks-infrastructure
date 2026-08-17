862b5d1f1dc1f96628448a0efd595d02a901811b
Calvin Harmon <calvinh0@gmail.com>
2026-08-16 23:24:03 -0500

    eks-add-ons

---
+++ modules/eks-addons/main.tf
@@ -0,0 +1,129 @@
+data "aws_partition" "current" {}
+
+data "aws_iam_policy_document" "pod_identity_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole", "sts:TagSession"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["pods.eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "vpc_cni" {
+  name               = "${var.cluster_name}-vpc-cni"
+  assume_role_policy = data.aws_iam_policy_document.pod_identity_assume_role.json
+  tags               = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_cni" {
+  role       = aws_iam_role.vpc_cni.name
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role" "ebs_csi" {
+  name               = "${var.cluster_name}-ebs-csi"
+  assume_role_policy = data.aws_iam_policy_document.pod_identity_assume_role.json
+  tags               = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi" {
+  role       = aws_iam_role.ebs_csi.name
+  policy_arn = "arn:${data.aws_partition.current.partition}:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicyV2"
+}
+
+data "aws_iam_policy_document" "ebs_kms" {
+  statement {
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:Encrypt",
+      "kms:GenerateDataKeyWithoutPlaintext",
+      "kms:ReEncrypt*"
+    ]
+    resources = [var.ebs_kms_key_arn]
+  }
+
+  statement {
+    actions   = ["kms:CreateGrant"]
+    resources = [var.ebs_kms_key_arn]
+    condition {
+      test     = "Bool"
+      variable = "kms:GrantIsForAWSResource"
+      values   = ["true"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "ebs_kms" {
+  name   = "ebs-kms"
+  role   = aws_iam_role.ebs_csi.id
+  policy = data.aws_iam_policy_document.ebs_kms.json
+}
+
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "eks-pod-identity-agent"
+  addon_version               = var.addon_versions["eks-pod-identity-agent"]
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+  tags                        = var.tags
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "vpc-cni"
+  addon_version               = var.addon_versions["vpc-cni"]
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+
+  pod_identity_association {
+    role_arn        = aws_iam_role.vpc_cni.arn
+    service_account = "aws-node"
+  }
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_iam_role_policy_attachment.vpc_cni
+  ]
+  tags = var.tags
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "coredns"
+  addon_version               = var.addon_versions["coredns"]
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+  tags                        = var.tags
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "kube-proxy"
+  addon_version               = var.addon_versions["kube-proxy"]
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+  tags                        = var.tags
+}
+
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "aws-ebs-csi-driver"
+  addon_version               = var.addon_versions["aws-ebs-csi-driver"]
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+
+  pod_identity_association {
+    role_arn        = aws_iam_role.ebs_csi.arn
+    service_account = "ebs-csi-controller-sa"
+  }
+
+  depends_on = [
+    aws_eks_addon.pod_identity_agent,
+    aws_iam_role_policy_attachment.ebs_csi,
+    aws_iam_role_policy.ebs_kms
+  ]
+  tags = var.tags
+}
